2329cbe4f16d8bc6a6fa467843d6b41cf55c6b71
Lincoln <phpgladiator@gmail.com>
2021-07-16 18:32:35 +0600

    demo 01 completed

--- classes/class-create-admin-menu.php
+++ classes/class-create-admin-menu.php
@@ -1,47 +1,60 @@
 <?php
+
 /**
  * This file will create admin menu page.
  */
 
-class AZ_MHT_Create_Admin_Page {
+class AZ_MHT_Create_Admin_Page
+{
 
-    public function __construct() {
-        add_action( 'admin_menu', [ $this, 'create_admin_menu' ] );
-    }
+	public function __construct()
+	{
+		add_action('admin_menu', [$this, 'create_admin_menu']);
+	}
 
-    public function create_admin_menu() {
-        $capability = 'manage_options';
-        $slug = 'wprk-settings';
+	public function create_admin_menu()
+	{
+		$capability = 'manage_options';
+		$slug = 'wprk-settings';
 
-        $mht_plist_menu_page = add_menu_page(
-            __( 'Master Price List', 'wp-react-kickoff' ),
-            __( 'Master Price List', 'wp-react-kickoff' ),
-            $capability,
-            $slug,
-            [ $this, 'menu_page_template' ],
-            'dashicons-screenoptions'
-        );
+		$mht_plist_menu_page = add_menu_page(
+			__('Master Price List', 'wp-react-kickoff'),
+			__('Master Price List', 'wp-react-kickoff'),
+			$capability,
+			$slug,
+			[$this, 'mht_master_plist_html'],
+			'dashicons-screenoptions'
+		);
 
-        add_action( 'load-' . $mht_plist_menu_page, [$this, 'load_admin_css'] );
+		add_action('load-' . $mht_plist_menu_page, [$this, 'load_admin_css']);
+	}
 
-    }
+	public function load_admin_css()
+	{
+		add_action('admin_enqueue_scripts', [$this, 'enqueue_admin_css']);
+	}
 
-    public function load_admin_css(){
-        add_action( 'admin_enqueue_scripts', [ $this, 'enqueue_admin_css' ] );
-    }
+	public function enqueue_admin_css()
+	{
+		// Isn't it nice to use dependencies and the already registered core js files?
+		wp_enqueue_style('mht-plist-css', MHT_PLIST_URL . '/assets/mht-plist.css', array(), wp_rand());
+	}
 
-    public function enqueue_admin_css(){
-        // Isn't it nice to use dependencies and the already registered core js files?
-        wp_enqueue_style( 'mht-plist-css', MHT_PLIST_URL . '/assets/mht-plist.css', array(), wp_rand() );
-    }
 
-    public function menu_page_template() {
-       // echo '<div class="wrap"><div id="wprk-admin-app"></div></div>';
-       ?>
+	public function mht_master_plist_html()
+	{
+		$this->print_pricelist_header();
+		$this->print_product_list_row('245', '75', '22.5'); //loop
+		$this->print_product_list_row('265', '75', '22.5'); //loop
+		$this->print_product_list_row('275', '80', '22.5'); //loop
 
-<style>
+		$this->print_pricelist_footer();
+	}
 
-</style>
+	public function menu_page_template()
+	{
+		// echo '<div class="wrap"><div id="wprk-admin-app"></div></div>';
+?>
 
 <h1>Price list of MHT Pricing</h1>
 
@@ -216,11 +229,6 @@ class AZ_MHT_Create_Admin_Page {
                             <div class="product-price">$20</div>
                             <div class="product-quentatty">5</div>
                         </div>
-                        <div class="product-detail-container">
-                            <div class="product-name">Product name</div>
-                            <div class="product-price">$20</div>
-                            <div class="product-quentatty">5</div>
-                        </div>
                     </div>
                 </div> <!-- .brand-item end -->
             </div><!-- .brand-items end -->
@@ -228,8 +236,184 @@ class AZ_MHT_Create_Admin_Page {
         </div> <!-- .tire-detail-container end -->
     </div><!-- .mht-plist-row end -->
 </div><!-- .mht-price-list end -->
-<?php
-    }
 
+<?php
+	}
+
+
+	function print_product_list_row($width, $ratio, $rim)
+	{
+		$MHT_MasterPriceList = new MHT_MasterPriceList();
+		$tire_size_item_arr = $MHT_MasterPriceList->tire_size_item_arr($width, $ratio, $rim);
+
+        if(empty($tire_size_item_arr['manufacturers'])){
+            return;
+        }
+	?>
+
+<div class="mht-plist-row">
+    <div class="tire-size plist-row-item">
+        <h3><?php echo $tire_size_item_arr['tire_size'] ?></h3>
+    </div>
+
+    <div class="tire-detail-container plist-row-item">
+
+        <div class="brand-items">
+            <?php
+					if (!empty($tire_size_item_arr['manufacturers'])) {
+						$manufacturers = $tire_size_item_arr['manufacturers'];
+
+						foreach ($manufacturers as $key => $manufacturer) {
+					?>
+            <div class="brand-item">
+                <div class="tire-manufacturer tire-set-item">
+                    <h3><?php echo  $manufacturer['name'] ?></h3>
+                </div>
+
+                <?php
+				$two_tire_set_items = !empty($tire_size_item_arr['manufacturers'][$key]['2_tire_set'])
+					? $tire_size_item_arr['manufacturers'][$key]['2_tire_set'] : [];
+				$this->print_tire_set_items(2, $two_tire_set_items);
+
+
+				$four_tire_set_items = !empty($tire_size_item_arr['manufacturers'][$key]['4_tire_set'])
+					? $tire_size_item_arr['manufacturers'][$key]['4_tire_set'] : [];
+				$this->print_tire_set_items(4, $four_tire_set_items);
+
+
+				$six_tire_set_items = !empty($tire_size_item_arr['manufacturers'][$key]['6_tire_set'])
+					? $tire_size_item_arr['manufacturers'][$key]['6_tire_set'] : [];
+				$this->print_tire_set_items(6, $six_tire_set_items);
+
+				$eight_tire_set_items = !empty($tire_size_item_arr['manufacturers'][$key]['8_tire_set'])
+					? $tire_size_item_arr['manufacturers'][$key]['8_tire_set'] : [];
+				$this->print_tire_set_items(8, $eight_tire_set_items);
+
+				?>
+
+            </div>
+            <!-- .brand-item end -->
+
+            <?php
+
+						}
+					}
+
+					?>
+
+        </div><!-- .brand-items end -->
+
+    </div> <!-- .tire-detail-container end -->
+</div><!-- .mht-plist-row end -->
+<?php
+
+	}
+
+
+
+	function print_tire_set_items($tire_set_numbar, $tire_set_items)
+	{
+	?>
+
+<div class="tire-set-item <?= $tire_set_numbar . '_tire_set' ?>">
+    <?php
+			if (!empty($tire_set_items)) {
+				foreach ($tire_set_items as $tire_set_item) {
+			?>
+    <div class="product-detail-container">
+        <div class="product-name">
+            <?= $tire_set_item['produt_name'] ?>
+        </div>
+        <div class="product-price">
+            <?= $tire_set_item['price'] ?>
+        </div>
+        <div class="product-quentatty">
+            <?= $tire_set_item['qty'] ?>
+        </div>
+    </div>
+    <?php
+
+				}
+			}
+
+			?>
+</div>
+
+<?php
+
+	}
+
+
+	function print_pricelist_header()
+	{
+	?>
+<h1>Price list of MHT Pricing</h1>
+<div class="mht-price-list">
+
+    <div class="mht-plist-row plist-header">
+        <div class="tire-size plist-row-item">
+            <h3>Tire Size</h3>
+        </div>
+        <div class="tire-detail-container plist-row-item">
+            <div class="tire-manufacturer tire-set-item">
+                <h3>Manufacturer</h3>
+            </div>
+
+            <div class="tire-set-item">
+                <div class="titel">
+                    <h3>2 tire set</h3>
+                </div>
+                <div class="pdetail-headings">
+                    <div class="product-name">Product name</div>
+                    <div class="product-price">Price</div>
+                    <div class="product-quentatty">Qty</div>
+                </div>
+            </div>
+
+            <div class="tire-set-item">
+                <div class="titel">
+                    <h3>4 tire set</h3>
+                </div>
+                <div class="pdetail-headings">
+                    <div class="product-name">Product name</div>
+                    <div class="product-price">Price</div>
+                    <div class="product-quentatty">Qty</div>
+                </div>
+            </div>
+
+            <div class="tire-set-item">
+                <div class="titel">
+                    <h3>6 tire set</h3>
+                </div>
+                <div class="pdetail-headings">
+                    <div class="product-name">Product name</div>
+                    <div class="product-price">Price</div>
+                    <div class="product-quentatty">Qty</div>
+                </div>
+            </div>
+
+            <div class="tire-set-item">
+                <div class="titel">
+                    <h3>8 tire set</h3>
+                </div>
+                <div class="pdetail-headings">
+                    <div class="product-name">Product name</div>
+                    <div class="product-price">Price</div>
+                    <div class="product-quentatty">Qty</div>
+                </div>
+            </div>
+
+        </div> <!-- .tire-detail-container end -->
+    </div><!-- .mht-plist-row end -->
+    <?php
+	}
+
+
+	function print_pricelist_footer()
+	{
+		?>
+</div><!-- .mht-price-list end -->
+<?php
+	}
 }
 new AZ_MHT_Create_Admin_Page();
---
+++ classes/class-mht-master-price-list.php
@@ -0,0 +1,178 @@
+<?php
+
+class MHT_MasterPriceList {
+
+
+    var $currency_symbol;
+
+    function __construct()
+    {
+        $this->currency_symbol = get_woocommerce_currency_symbol();
+    }
+
+    public function register_actions(){
+        add_filter( 'woocommerce_product_data_store_cpt_get_products_query', [$this, 'handle_custom_query_var'], 10, 2 );
+        add_action('template_redirect', [$this, 'test']);
+    }
+
+    function test(){
+        //255 75 22.5 shuld not reurn any item
+        echo '<pre>';
+        print_r($this->tire_size_item_arr( '245', '75', '22.5' ));
+        die();
+    }
+
+    /**
+     * Handle a custom 'customvar' query var to get products with the 'customvar' meta.
+     * @param array $query - Args for WP_Query.
+     * @param array $query_vars - Query vars from WC_Product_Query.
+     * @return array modified $query
+     */
+    function handle_custom_query_var( $query, $query_vars ) {
+
+        if ( ! empty( $query_vars['mht_custom_query_price_list'] ) ) {
+
+            $query['meta_query'][] = array(
+                'relation' => 'AND',
+                array(
+                    'key' => 'width',
+                    'value' => $query_vars['mht_custom_query_price_list']['width'],
+                    'compare' => '=',
+                ),
+                array(
+                    'key' => 'ratio',
+                    'value' => $query_vars['mht_custom_query_price_list']['ratio'],
+                    'compare' => '=',
+                ),
+                array(
+                    'key' => 'rim',
+                    'value' => $query_vars['mht_custom_query_price_list']['rim'],
+                    'compare' => '=',
+                )
+                );
+
+        }
+
+        //print_r($query_vars);
+
+        return $query;
+    }
+
+
+    function get_manufacturer_name_by_index( $index ) {
+        $options_arr = array(
+            '0'  => __('--none--', 'woocommerce'),
+            '1'  => __('BF Goodrich', 'woocommerce'),
+            '2'  => __('Boto', 'woocommerce'),
+            '3'  => __('Bridgestone', 'woocommerce'),
+            '4'  => __('Continental', 'woocommerce'),
+            '5'  => __('Double Coin', 'woocommerce'),
+            '6'  => __('Duraturn', 'woocommerce'),
+            '7'  => __('DynaTrac', 'woocommerce'),
+            '8'  => __('Firestone', 'woocommerce'),
+            '9'  => __('General', 'woocommerce'),
+            '10' => __('Goodyear', 'woocommerce'),
+            '11' => __('Hankook', 'woocommerce'),
+            '12' => __('Hercules', 'woocommerce'),
+            '13' => __('Ironman', 'woocommerce'),
+            '14' => __('Leao', 'woocommerce'),
+            '15' => __('Michelin', 'woocommerce'),
+            '16' => __('Roadlux', 'woocommerce'),
+            '17' => __('Roadmaster', 'woocommerce'),
+            '18' => __('Sailun', 'woocommerce'),
+            '19' => __('Samson', 'woocommerce'),
+            '20' => __('Sierra', 'woocommerce'),
+            '21' => __('Sumitomo', 'woocommerce'),
+            '22' => __('Super Cargo', 'woocommerce'),
+            '23' => __('Toyo', 'woocommerce'),
+            '24' => __('Westlake', 'woocommerce'),
+            '25' => __('Yokohama', 'woocommerce')
+        );
+
+        return $options_arr[$index];
+    }
+
+
+    public static function get_producs_by_size($width, $ratio, $rim ){
+
+        $products = wc_get_products( array(
+            'limit' => -1,
+            'mht_custom_query_price_list' => array(
+            'width' => $width,
+            'ratio' => $ratio,
+            'rim' =>  $rim
+        ) ) );
+
+        return $products;
+    }
+
+
+    /**
+     * Return a array item for a particular tire size
+     *
+     * @param [type] $width
+     * @param [type] $ratio
+     * @param [type] $rim
+     * @return void
+     */
+    public function tire_size_item_arr( $width, $ratio, $rim ){
+
+        $tire_size_str = $width .'/'.$ratio . 'R' . $rim;
+
+        $tire_size_item_arr = [
+            'tire_size' => $tire_size_str
+        ];
+
+        $products = $this->get_producs_by_size($width, $ratio, $rim);
+
+        if(empty($products)) {
+            $tire_size_item_arr['manufacturers'] = [];
+
+            return $tire_size_item_arr;
+        }
+
+
+        $tire_size_item_arr['manufacturers'] = $this->get_manufacturers_arr($products);
+
+        return $tire_size_item_arr;
+
+    }
+
+
+    public function get_manufacturers_arr($products){
+        //echo count($products);
+        $manufacturers = [];
+
+        foreach($products as $product){
+            $manufacturer_id = get_post_meta($product->get_id(), 'tyerBrand', true);
+            $tire_set_number = get_post_meta($product->get_id(), 'number_selling_tires', true);
+
+			$product_edit_link = get_edit_post_link($product->get_id());
+			$product_title = $product->get_title();
+			$product_name = '<a href="'.$product_edit_link.'" target="_blank">'.$product_title.'</a>';
+            $product_item_arr = [
+                'produt_name' => $product_name,
+                'price' => $this->currency_symbol. $product->get_price(),
+                'qty'   => $product->get_stock_quantity()
+            ];
+
+            if( empty($manufacturers[$manufacturer_id]['name'] )) {
+                $manufacturers[$manufacturer_id]['name'] =  $this->get_manufacturer_name_by_index($manufacturer_id);
+            }
+
+           if( empty($manufacturers[$manufacturer_id][ $tire_set_number . '_tire_set' ]) ) {
+            $manufacturers[$manufacturer_id][ $tire_set_number . '_tire_set' ] = [];
+           }
+
+            array_push($manufacturers[$manufacturer_id][ $tire_set_number . '_tire_set' ], $product_item_arr);
+
+        }
+
+        return $manufacturers;
+    }
+
+}
+
+ $MHT_MasterPriceList = new MHT_MasterPriceList();
+
+ $MHT_MasterPriceList->register_actions();
