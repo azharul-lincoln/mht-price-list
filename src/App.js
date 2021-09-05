fab5be74b8770c29c472ecbe24c19ff3f75d643f
Lincoln <phpgladiator@gmail.com>
2021-09-05 20:20:55 +0600

    5 sep work done

--- src/App.js
+++ src/App.js
@@ -1,17 +1,103 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import Skeleton from "@yisheng90/react-loading";
+import axios from "axios";
+
 import { Header } from "./components/Header/Header";
 import Body from "./components/Body";
 import HeaderTop from "./components/Header/HeaderTop";
 
+function getCostBySize(products) {
+  let newProducts = [];
+
+  products.forEach((product) => {
+    //console.log(product);
+    const { tire_size, manufacturers } = product;
+
+    const manKeyes = Object.keys(manufacturers);
+    let newProduct = [];
+    newProduct["tire_size"] = tire_size;
+    newProduct["id"] = tire_size.replace(/\W/g, "_");
+    newProduct["cost_by_manufacturers"] = [];
+
+    manKeyes.forEach((key, index) => {
+      if (!manufacturers[key].hasOwnProperty("cost")) {
+        manufacturers[key].cost = 0;
+      }
+      newProduct["cost_by_manufacturers"].push({
+        name: key,
+        cost: manufacturers[key].cost,
+      });
+      //console.log(key, manufacturers[key], tire_size);
+    });
+    newProducts.push(newProduct);
+  });
+
+  console.log("newProducts:", newProducts);
+  return newProducts;
+}
+
 function App() {
+  const [costsByTireSize, setCostsByTireSize] = useState();
+  const [changes, setChanges] = useState("Save Changes");
+
+  const url = `${appLocalizer.apiUrl}/mpl/v1/settings`;
+
+  useEffect(() => {
+    axios.get(url).then((res) => {
+      // setFirstName( res.data.firstname );
+      // setLastName( res.data.lastname );
+      // setEmail( res.data.email );
+      console.log("res", res);
+
+      if (res.data.products == false) {
+        console.log("empty products");
+
+        const product_to_send_wp = JSON.stringify(products);
+
+        axios
+          .post(
+            url,
+            {
+              products: product_to_send_wp,
+            },
+            {
+              headers: {
+                "content-type": "application/json",
+                "X-WP-NONCE": appLocalizer.nonce,
+              },
+            }
+          )
+          .then((res) => {
+            //setLoader("Save Settings");
+            console.log("settigs updated!", res);
+          });
+      } else {
+        let testProducts = JSON.parse(res.data.products);
+        console.log("testProducts", testProducts);
+      }
+    });
+  }, []);
+
+  const products = appLocalizer.products;
+  //console.log(products);
+
+  const costBySize = getCostBySize(products);
+
+  console.log("CP", costBySize);
+
   return (
     <React.Fragment>
       <HeaderTop />
       <div className="mht-price-list">
         <Header />
-        <Body />
+        <Body products={products} />
       </div>
     </React.Fragment>
+
+    // <React.Fragment>
+    //   <div style={{ fontSize: "22px", margin: "20px 0" }}>Loading...</div>
+    //   <Skeleton width={"100%"} height={60} rows={20} color={"#ddd"} />
+    // </React.Fragment>
   );
 }
 export default App;
--- src/components/Body.js
+++ src/components/Body.js
@@ -1,101 +1,115 @@
 import React, { useState, useEffect } from "react";
-import axios from "axios";
+// import axios from "axios";
 
 import PriceListRow from "./PriceListRow";
 
-function setCostBySize(products) {
-  console.log("products", products);
+// function getCostBySize(products) {
+//   let newProducts = [];
 
-  let costBySize = [];
+//   products.forEach((product) => {
+//     //console.log(product);
+//     const { tire_size, manufacturers } = product;
 
-  products.forEach((product) => {
-    let item = [];
-    item["tire_size"] = product.tire_size;
-    item["manufacturers"] = [];
-    item["manufacturers_key"] = Object.keys(product.manufacturers);
+//     const manKeyes = Object.keys(manufacturers);
+//     let newProduct = [];
+//     newProduct["tire_size"] = tire_size;
+//     newProduct["id"] = tire_size.replace(/\W/g, "_");
+//     newProduct["cost_by_manufacturers"] = [];
 
-    item.manufacturers_key.forEach((manufacturer) => {
-      item["manufacturers"].push((item["manufacturers"][manufacturer] = 0));
-    });
-    console.log(item);
+//     manKeyes.forEach((key, index) => {
+//       if (!manufacturers[key].hasOwnProperty("cost")) {
+//         manufacturers[key].cost = 0;
+//       }
+//       newProduct["cost_by_manufacturers"].push({
+//         name: key,
+//         cost: manufacturers[key].cost,
+//       });
+//       //console.log(key, manufacturers[key], tire_size);
+//     });
+//     newProducts.push(newProduct);
+//   });
 
-    // if (product.manufacturers) {
-    //   product.manufacturers.forEach(function(manufacturer, index) {
-    //     console.log("manufacturer", manufacturer, index);
-    //   });
-    // }
-  });
-}
+//   console.log("newProducts:", newProducts);
+//   return newProducts;
+// }
 
-const Body = () => {
-  const [ppp, setPpp] = useState("");
-  const [changes, setChanges] = useState("Save Changes");
+const Body = ({ products }) => {
+  //   const [costsByTireSize, setCostsByTireSize] = useState();
+  //   const [changes, setChanges] = useState("Save Changes");
 
-  const url = `${appLocalizer.apiUrl}/mpl/v1/settings`;
+  //   const url = `${appLocalizer.apiUrl}/mpl/v1/settings`;
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    setLoader("Saving...");
-    axios
-      .post(
-        url,
-        {
-          firstname: firstname,
-          lastname: lastname,
-          email: email,
-        },
-        {
-          headers: {
-            "content-type": "application/json",
-            "X-WP-NONCE": appLocalizer.nonce,
-          },
-        }
-      )
-      .then((res) => {
-        setLoader("Save Settings");
-      });
-  };
+  //   const handleSubmit = (e) => {
+  //     e.preventDefault();
+  //     setLoader("Saving...");
+  //     axios
+  //       .post(
+  //         url,
+  //         {
+  //           firstname: firstname,
+  //           lastname: lastname,
+  //           email: email,
+  //         },
+  //         {
+  //           headers: {
+  //             "content-type": "application/json",
+  //             "X-WP-NONCE": appLocalizer.nonce,
+  //           },
+  //         }
+  //       )
+  //       .then((res) => {
+  //         setLoader("Save Settings");
+  //       });
+  //   };
 
-  useEffect(() => {
-    axios.get(url).then((res) => {
-      // setFirstName( res.data.firstname );
-      // setLastName( res.data.lastname );
-      // setEmail( res.data.email );
-      console.log("res", res);
+  //   useEffect(() => {
+  //     axios.get(url).then((res) => {
+  //       // setFirstName( res.data.firstname );
+  //       // setLastName( res.data.lastname );
+  //       // setEmail( res.data.email );
+  //       console.log("res", res);
 
-      if (res.data.products == false) {
-        console.log("empty products");
+  //       if (res.data.products == false) {
+  //         console.log("empty products");
 
-        const product_to_send_wp = JSON.stringify(products);
+  //         const product_to_send_wp = JSON.stringify(products);
 
-        axios
-          .post(
-            url,
-            {
-              products: product_to_send_wp,
-            },
-            {
-              headers: {
-                "content-type": "application/json",
-                "X-WP-NONCE": appLocalizer.nonce,
-              },
-            }
-          )
-          .then((res) => {
-            //setLoader("Save Settings");
-            console.log("settigs updated!", res);
-          });
-      } else {
-        let testProducts = JSON.parse(res.data.products);
-        console.log("testProducts", testProducts);
-      }
-    });
-  }, []);
+  //         axios
+  //           .post(
+  //             url,
+  //             {
+  //               products: product_to_send_wp,
+  //             },
+  //             {
+  //               headers: {
+  //                 "content-type": "application/json",
+  //                 "X-WP-NONCE": appLocalizer.nonce,
+  //               },
+  //             }
+  //           )
+  //           .then((res) => {
+  //             //setLoader("Save Settings");
+  //             console.log("settigs updated!", res);
+  //           });
+  //       } else {
+  //         let testProducts = JSON.parse(res.data.products);
+  //         console.log("testProducts", testProducts);
+  //       }
+  //     });
+  //   }, []);
 
-  const products = appLocalizer.products;
-  //console.log(products);
+  //   const products = appLocalizer.products;
+  //   //console.log(products);
 
-  const costBySize = setCostBySize(products);
+  //   const costBySize = getCostBySize(products);
+
+  //   console.log("CP", costBySize);
+
+  //12/R22.5
+
+  //const test = costBySize.filter((item) => item.tire_size == "255/70R22.5");
+
+  //console.log("test", test);
 
   return (
     <React.Fragment>
